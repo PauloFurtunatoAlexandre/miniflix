144f3c52d42ad031cafc7cf8e73d8014195ac0c7
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2018-03-11 08:59:26 -0600

    corrected https of cloudnary

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -3,37 +3,42 @@ import { Link } from 'react-router';
 import Nav from './Nav';
 
 class Upload extends Component {
-
   state = { jokes: [] };
 
   uploadWidget = () => {
     window.cloudinary.openUploadWidget(
-      { cloud_name: 'miniflix-project',
-      upload_preset: 'gbwztkqu',
-      tags: ['miniflix']
-    },
-    function(error, result) {
-      console.log("This is the result of the last upload", result);
-    });
-  }
+      {
+        cloud_name: 'miniflix-project',
+        upload_preset: 'gbwztkqu',
+        tags: ['miniflix']
+      },
+      function(error, result) {
+        console.log('This is the result of the last upload', result);
+      }
+    );
+  };
 
   render() {
-
     const { jokes } = this.state;
 
     return (
       <div>
-      <Nav />
-      <h3 className="text-center">Upload Your 20-second Video in a blink of an Eye</h3>
-      <hr/>
+        <Nav />
+        <h3 className="text-center">
+          Upload Your 20-second Video in a blink of an Eye
+        </h3>
+        <hr />
 
-      <div className="col-sm-12">
-      <div className="jumbotron text-center">
-      <button onClick={this.uploadWidget} className="btn btn-lg btn-info"> Upload Video</button>
+        <div className="col-sm-12">
+          <div className="jumbotron text-center">
+            <button onClick={this.uploadWidget} className="btn btn-lg btn-info">
+              {' '}
+              Upload Video
+            </button>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-      );
+    );
   }
 }
 
